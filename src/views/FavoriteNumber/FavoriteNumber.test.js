e7de3ccc56391016c8cf9c491a052ba42c5b928a
Gabe Bravo <gabecbravo@gmail.com>
2020-03-27 14:32:29 -0400

    Basic example setting up fireEvent handler for an onChange on am input

--- src/views/FavoriteNumber/FavoriteNumber.test.js
+++ src/views/FavoriteNumber/FavoriteNumber.test.js
@@ -1,10 +1,23 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
 import FavoriteNumber from '.';
 
+// DEBUGGING EXAMPLE ______________________________
 test('renders a number input with a label "Favorite Number"', () => {
   const { getByLabelText, debug } = render(<FavoriteNumber />); // getting a query method from render method
   const input = getByLabelText(/favorite number/i); // regex ignore case
-  debug(input); // will print out the container we are rendering
+  // debug(input); // will print out the container we are rendering
   expect(input).toHaveAttribute('type', 'number'); // toHaveAttribute (atribute, value)
+});
+
+// FIRING EVENTS WITH RTL ______________________________
+test('entering an invalid value shows an error message', () => {
+  const { getByLabelText, getByRole } = render(<FavoriteNumber />);
+  const input = getByLabelText(/favorite number/i);
+
+  // this will take the input, which is the target, and apply all of the properties we've specified.
+  // So now the input is going to have a value of 10.
+  fireEvent.change(input, { target: { value: '10' } });
+
+  expect(getByRole('alert')).toHaveTextContent(/the number is invalid/i);
 });

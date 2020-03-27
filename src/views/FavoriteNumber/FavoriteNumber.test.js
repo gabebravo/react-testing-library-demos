37af879e89b489ce5cafcd24eaf458fc7092851e
Gabe Bravo <gabecbravo@gmail.com>
2020-03-27 13:15:22 -0400

    Final simple and clean version using the render method from @testing-library/react

--- src/views/FavoriteNumber/FavoriteNumber.test.js
+++ src/views/FavoriteNumber/FavoriteNumber.test.js
@@ -1,21 +1,9 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import { getQuereiesForElement } from '@testing-library/dom';
+import { render } from '@testing-library/react';
 import FavoriteNumber from '.';
 
-// REFACTOR __________________________________
-
-// DRY - can reuse for any React DOM testing instance
-function render(ui) {
-  const container = document.createElement('div');
-  ReactDOM.render(ui, container); // wraps ui in container
-  const queries = getQuereiesForElement(container); // gets all query methods
-  return { container, ...queries }; // return container and methods
-}
-
 test('renders a number input with a label "Favorite Number"', () => {
-  const { getByLabelText } = render(<FavoriteNumber />); // getting a query method
+  const { getByLabelText } = render(<FavoriteNumber />); // getting a query method from render method
   const input = getByLabelText(/favorite number/i); // regex ignore case
-  // toHaveAttribute (atribute, value)
-  expect(input).toHaveAttribute('type', 'number');
+  expect(input).toHaveAttribute('type', 'number'); // toHaveAttribute (atribute, value)
 });

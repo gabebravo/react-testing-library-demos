aed96afb478ee99cf19ca664863afdc39719eb02
Gabe Bravo <gabecbravo@gmail.com>
2020-03-27 16:15:00 -0400

    Used queryByRole to verify an element is not being rendered.  Max prop change (10) will render null.

--- src/views/FavoriteNumber/FavoriteNumber.test.js
+++ src/views/FavoriteNumber/FavoriteNumber.test.js
@@ -23,20 +23,9 @@ test('entering an invalid value shows an error message', () => {
   expect(getByRole('alert')).toHaveTextContent(/the number is invalid/i);
 });
 
-// ALT VERSION USING USER FROM RTL ______________________________
-test('alt version using the user type method from RTL User', () => {
-  const { getByLabelText, getByRole } = render(<FavoriteNumber />);
-  const input = getByLabelText(/favorite number/i);
-
-  // the user here is simulating typing out the keys and pressing the number 1 and 0.
-  user.type(input, '10');
-
-  expect(getByRole('alert')).toHaveTextContent(/the number is invalid/i);
-});
-
 // TEST VARIATION USING THE RERENDER METHOD ______________________
-test('test variation using the rerender method', () => {
-  const { getByLabelText, getByRole, rerender, debug } = render(
+test('test variation with updating max prop to 10', () => {
+  const { getByLabelText, getByRole, rerender, queryByRole, debug } = render(
     <FavoriteNumber />
   );
   const input = getByLabelText(/favorite number/i);
@@ -50,4 +39,12 @@ test('test variation using the rerender method', () => {
   debug();
   rerender(<FavoriteNumber max={10} />);
   debug();
+
+  // this wont work with getByRole needs to be queryByRole
+  // If you want to verify an element is not being rendered, then you're going to use a query that is prefixed with 'query' (queryByRole) rather than one that is prefixed with 'get' (getByRole).
+  expect(queryByRole('alert')).toBeNull();
+
+  // the reason is because the role doesnt exist because the div with the alert isnt rendering
+  // here is the logic : {isValid ? null : <div role="alert">The number is invalid</div>}
+  // so queryByRole will do the query, not find it, and thus confirm it is null
 });

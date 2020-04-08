c86c21fb75c64b142b673ddad8dceca708391b70
Gabe Bravo <gabecbravo@gmail.com>
2020-04-07 22:55:36 -0400

    Added the Redux Counter test

---
+++ src/views/ReduxCounter/ReduxCounter.test.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Provider } from 'react-redux';
+import { render, fireEvent } from '@testing-library/react';
+import ReduxCounter from '.';
+import { store } from '../../redux/store';
+
+test('can render with redux with defaults', () => {
+  // renders the redux wrapped component
+  const { getByLabelText, getByText } = render(
+    <Provider store={store}>
+      <ReduxCounter />
+    </Provider>
+  );
+
+  // fires the click event to increment
+  fireEvent.click(getByText('+'));
+  // asserts the count is now 1
+  expect(getByLabelText(/count/i)).toHaveTextContent('1');
+});

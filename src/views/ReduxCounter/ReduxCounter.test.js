a252de62ac41cf16e88dfb2fae57bbf0d1ae504b
Gabe Bravo <gabecbravo@gmail.com>
2020-04-07 23:19:46 -0400

    Mock example using default state for redux

--- src/views/ReduxCounter/ReduxCounter.test.js
+++ src/views/ReduxCounter/ReduxCounter.test.js
@@ -1,13 +1,15 @@
 import React from 'react';
+import { createStore } from 'redux';
 import { Provider } from 'react-redux';
 import { render, fireEvent } from '@testing-library/react';
 import ReduxCounter from '.';
-import { store } from '../../redux/store';
+import { store as appStore } from '../../redux/store';
+import { reducer } from '../../redux/reducer';
 
 test('can render with redux with defaults', () => {
   // renders the redux wrapped component
   const { getByLabelText, getByText } = render(
-    <Provider store={store}>
+    <Provider store={appStore}>
       <ReduxCounter />
     </Provider>
   );
@@ -17,3 +19,19 @@ test('can render with redux with defaults', () => {
   // asserts the count is now 1
   expect(getByLabelText(/count/i)).toHaveTextContent('1'); // span has an aria-label
 });
+
+// test('can render with redux with custom initial state', () => {
+//   // using createStore to set initial state;
+//   const store = createStore(reducer, { count: 3 });
+//   // renders the redux wrapped component with store instance that has default state
+//   const { getByLabelText, getByText, debug } = render(
+//     <Provider store={store}>
+//       <ReduxCounter />
+//     </Provider>
+//   );
+
+//   // fires the click event to decrement
+//   fireEvent.click(getByText('-')); // targets the button text
+//   // asserts the count is now 2, based on initial state being 3
+//   expect(getByLabelText(/count/i)).toHaveTextContent('2'); // span has an aria-label
+// });

e7f312e2008249fb99cd0fd0baf7194db0f73a88
Gabe Bravo <gabecbravo@gmail.com>
2020-04-07 23:24:57 -0400

    Added the repetative redux wrapper util for testing

--- src/views/ReduxCounter/ReduxCounter.test.js
+++ src/views/ReduxCounter/ReduxCounter.test.js
@@ -35,3 +35,45 @@ test('can render with redux with defaults', () => {
 //   // asserts the count is now 2, based on initial state being 3
 //   expect(getByLabelText(/count/i)).toHaveTextContent('2'); // span has an aria-label
 // });
+
+// _____________________________ REDUX WRAPPER UTIL FOR REPETATIVE TESTS
+
+// this is a handy function that I normally make available for all my tests
+// that deal with connected components.
+// you can provide initialState or the entire store that the ui is rendered with
+// function render(
+//   ui,
+//   {
+//     initialState,
+//     store = createStore(reducer, initialState),
+//     ...renderOptions
+//   } = {}
+// ) {
+//   function Wrapper({ children }) {
+//     return <Provider store={store}>{children}</Provider>;
+//   }
+//   return {
+//     ...rtlRender(ui, {
+//       wrapper: Wrapper,
+//       ...renderOptions
+//     }),
+//     // adding `store` to the returned utilities to allow us
+//     // to reference it in our tests (just try to avoid using
+//     // this to test implementation details).
+//     store
+//   };
+// }
+
+// test('can increment the value', () => {
+//   const { getByLabelText, getByText } = render(<ReduxCounter />);
+//   fireEvent.click(getByText('+'));
+//   expect(getByLabelText(/count/i)).toHaveTextContent('1');
+// });
+
+// test('can decrement the value', () => {
+//   const { getByLabelText, getByText } = render(<ReduxCounter />, {
+//     initialState: { count: 3 }
+//   });
+//   fireEvent.click(getByText('-'));
+//   expect(getByLabelText(/count/i)).toHaveTextContent('2');
+// });

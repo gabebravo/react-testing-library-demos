3fb6f8f3ea5edce60f6e306016cb2615d8b5a862
Gabe Bravo <gabecbravo@gmail.com>
2020-04-08 22:05:29 -0400

    Added a couple more tests for initial hook values and args tests

--- src/views/CustomHook/CustomHook.test.js
+++ src/views/CustomHook/CustomHook.test.js
@@ -23,3 +23,19 @@ test('exposes the count and increment/decrement functions', () => {
   act(() => result.decrement());
   expect(result.count).toBe(0);
 });
+
+test('allows customization of the initial count', () => {
+  const result = setup({ initialProps: { initialCount: 3 } });
+  expect(result.current.count).toBe(3);
+});
+
+test('allows customization of the step', () => {
+  const result = setup({ initialProps: { step: 2 } });
+  expect(result.current.count).toBe(0);
+
+  act(() => result.current.increment());
+  expect(result.current.count).toBe(2);
+
+  act(() => result.current.decrement());
+  expect(result.current.count).toBe(0);
+});
